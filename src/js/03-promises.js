27e95a6612eee68a107e1350f559fa67ae16a52b
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-06-19 20:54:14 +0300

    add task#3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+
+
+const form = document.querySelector(".form")
+
+form.addEventListener("submit", onSubmitForm)
+
+function onSubmitForm(event) {
+  event.preventDefault()
+  const { delay, step, amount } = event.target.elements
+  let delayStep = Number(delay.value)
+for (let i = 0; i < amount.value; i+=1) {
+createPromise(i, delayStep)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  delayStep += Number(step.value)
+}
+}
+
 function createPromise(position, delay) {
+  return new Promise ((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
   if (shouldResolve) {
-    // Fulfill
+    resolve({position, delay})
   } else {
-    // Reject
+    reject({position, delay})
   }
+  }, delay)
+  }
+  )
 }
