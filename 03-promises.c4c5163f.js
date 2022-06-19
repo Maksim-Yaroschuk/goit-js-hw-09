76baf75ef323357c401d3cb2e56c1edea42ef8dd
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-06-19 18:48:19 +0000

    Deploying to gh-pages from @ Maksim-Yaroschuk/goit-js-hw-09@7005e7ffcf2b8b8fa2ec2c0c47dca8e275db1690 🚀

--- 03-promises.c4c5163f.js
+++
@@ -1,2 +0,0 @@
-function e(e,t){return new Promise(((o,n)=>{const l=Math.random()>.3;setTimeout((()=>{l?o({position:e,delay:t}):n({position:e,delay:t})}),t)}))}document.querySelector(".form").addEventListener("submit",(function(t){t.preventDefault();const{delay:o,step:n,amount:l}=t.target.elements;let i=Number(o.value);for(let t=0;t<l.value;t+=1)e(t,i).then((({position:e,delay:t})=>{console.log(`✅ Fulfilled promise ${e} in ${t}ms`)})).catch((({position:e,delay:t})=>{console.log(`❌ Rejected promise ${e} in ${t}ms`)})),i+=Number(n.value)}));
-//# sourceMappingURL=03-promises.c4c5163f.js.map
