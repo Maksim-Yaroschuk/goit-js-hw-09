432ca1688876106dac1de331af8ffed0f9644b37
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-06-19 22:26:46 +0300

    add style from js, add message to finish timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -8,6 +8,9 @@ const days = document.querySelector('.value[data-days]')
 const hours = document.querySelector('.value[data-hours]')
 const minutes = document.querySelector('.value[data-minutes]')
 const seconds = document.querySelector('.value[data-seconds]')
+const timerStyle = document.querySelector('.timer')
+const fieldStyle = document.querySelectorAll('.field')
+const valueStyle = document.querySelectorAll('.value')
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -25,9 +28,10 @@ const fp = flatpickr(dateTime, options);
 let timerId = null
 let ms = 0
 
+addStyle()
+
 startBtn.setAttribute("disabled", true)
 startBtn.addEventListener("click", onStartCountdown)
-
 
 function onStartCountdown() {
     timerId = setInterval(() => {
@@ -37,7 +41,8 @@ function onStartCountdown() {
         minutes.textContent = addLeadingZero(`${convertMs(ms).minutes}`)
         seconds.textContent = addLeadingZero(`${convertMs(ms).seconds}`)
         if (ms <= 1000) {
-           clearInterval(timerId)
+            clearInterval(timerId)
+            Notiflix.Report.success('Finish!', 'Timer to finish!!!', 'OK');
         }
     }, 1000)
 }
@@ -58,4 +63,22 @@ function convertMs(ms) {
 
 function addLeadingZero(value) {
    return value.padStart(2, "0")  
+}
+
+function addStyle() {
+    timerStyle.style = `  
+        margin-top: 10px;
+        display: flex;` 
+    for (const el of fieldStyle) {
+    el.style = `  
+        font-weight: 500;
+        display: grid;
+        text-transform: uppercase;
+        padding: 5px;
+        text-align: center;` 
+    }
+    for (const el of valueStyle) {
+    el.style = `  
+        font-size: 30px` 
+    }
 }
